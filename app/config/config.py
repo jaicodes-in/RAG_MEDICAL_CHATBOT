694c0c0a2c54c39fb7b816700271147760efa8e1
jaipanchal <jaipanchal97@gmail.com>
2025-08-10 13:42:58 +0530

    chnaged model name

--- app/config/config.py
+++ app/config/config.py
@@ -2,7 +2,7 @@ import os
 
 HF_TOKEN=os.getenv("HF_TOKEN")
 
-HUGGINGFACE_REPO_ID="mistralai/Mistral-7B-Instruct"
+HUGGINGFACE_REPO_ID="mistralai/Mistral-7B-Instruct-v0.3"
 DB_FAISS_PATH='vector/db_faiss'
 DATA_PATH='data/'
 
